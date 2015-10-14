e854ba225029abf50739b2c463754a32ceead0ad
shelviadh <shelvia.ho@gmail.com>
2015-10-15 01:27:45 +0800

    deleted CommandResult and the child classes

--- Doowado - Task Manager/Doowado - Task Manager/Controller.cpp
+++ Doowado - Task Manager/Doowado - Task Manager/Controller.cpp
@@ -15,7 +15,8 @@ int main(int argc, char* argv[]) {
 	Parser parser;
 	string input;
 	static ptime currentTime(second_clock::local_time());
-	static Display displayList;
+	Display displayList;
+	vector<string>* commandResult;
 
 	LocalStorage.loadFromFile();
 	LocalStorage.saveToFile();

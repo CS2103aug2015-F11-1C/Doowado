58c2a74162c2343e7af7c98e8df9898974210244
dipti <diptivm08@gmail.com>
2015-10-17 11:38:11 +0800

    Added and configured CommandTest project

---
+++ Doowado - Task Manager/CommandTest/ShowCommandTest.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CommandTest
+{		
+	TEST_CLASS(ShowCommandTest)
+	{
+	public:
+		
+		TEST_METHOD(ConstructorTest)
+		{
+			date testDate;
+		}
+
+	};
+} 

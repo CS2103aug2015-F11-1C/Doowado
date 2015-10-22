dafc75b38141c06c463bb44d336d09df5958da4c
shelviadh <shelvia.ho@gmail.com>
2015-10-22 15:49:02 +0800

    implemented EditCommand to allow user to change title, startDate and startTime

--- Doowado - Task Manager/Doowado - Task Manager/EditCommand.cpp
+++ Doowado - Task Manager/Doowado - Task Manager/EditCommand.cpp
@@ -1,15 +1,15 @@
 #include "EditCommand.h"
 
-EditCommand::EditCommand(string entryType, int displayIndex, string newTitle, ptime newStartTime, ptime newEndTime, ptime newDueTime)
+EditCommand::EditCommand(EntryType entryType, int taskID, string newTitle, ptime newStartDate, ptime newStartTime, ptime newEndDate, ptime newEndTime)
 {
-	string _entryType = entryType;
-	int _taskId = displayIndex;
-	string _newTitle = newTitle;
-	ptime _newStartTime = newStartTime;
-	ptime _newEndTime = newEndTime;
-	ptime _newDueTime = newDueTime;
+	_entryType = entryType;
+	_taskID = taskID;
+	_newTitle = newTitle;
+	_newStartDate = newStartDate;
+	_newStartTime = newStartTime;
+	_newEndDate = newEndDate;
+	_newEndTime = newEndTime;
 }
-
 
 EditCommand::~EditCommand()
 {
@@ -17,12 +17,59 @@ EditCommand::~EditCommand()
 
 void EditCommand::execute(Storage* data, Display *display)
 {
-	if (_entryType == eventType) {
+	if (_entryType == event) {
 		Event* eventEntry = display->retrieveEvent(_taskID);
-		eventEntry->setName(_newTitle);
+		
+		if (_newTitle != "") {
+			eventEntry->setName(_newTitle);
+		}
+		if (!_newStartDate.is_not_a_date_time()) {
+			date startDate = _newStartDate.date();
+			if (!_newStartTime.is_not_a_date_time()) {
+				time_duration startTime = _newStartTime.time_of_day();
+				ptime editStartTime(startDate, startTime);
+				eventEntry->setStartTime(editStartTime);
+			}
+			else {
+				ptime originalStartTime = eventEntry->getStartTime();
+				time_duration startTime = originalStartTime.time_of_day();
+				ptime editStartTime(startDate, startTime);
+				eventEntry->setStartTime(editStartTime);
+			}
+		}
+		else if (!_newStartTime.is_not_a_date_time()) {
+			time_duration startTime = _newStartTime.time_of_day();
+			ptime originalStartDate = eventEntry->getStartTime();
+			date startDate = originalStartDate.date();
+			ptime editStartTime(startDate, startTime);
+			eventEntry->setStartTime(editStartTime);
+		}
+		/*
+		if (!_newEndDate.is_not_a_date_time()) {
+			date endDate = _newEndDate.date();
+			if (!_newStartTime.is_not_a_date_time()) {
+				time_duration startTime = _newStartTime.time_of_day();
+				ptime editStartTime(startDate, startTime);
+				eventEntry->setStartTime(editStartTime);
+			}
+			else {
+				ptime originalStartTime = eventEntry->getStartTime();
+				time_duration startTime = originalStartTime.time_of_day();
+				ptime editStartTime(startDate, startTime);
+				eventEntry->setStartTime(editStartTime);
+			}
+		}
+		else if (!_newStartTime.is_not_a_date_time()) {
+			time_duration startTime = _newStartTime.time_of_day();
+			ptime originalStartDate = eventEntry->getStartTime();
+			date startDate = originalStartDate.date();
+			ptime editStartTime(startDate, startTime);
+			eventEntry->setStartTime(editStartTime);
+		}
+		*/
 	}
 
-	else if (_entryType == taskType) {
+	else if (_entryType == task) {
 		Task* taskEntry = display->retrieveTask(_taskID);
 		taskEntry->setName(_newTitle);
 	}
--- Doowado - Task Manager/Doowado - Task Manager/EditCommand.h
+++ Doowado - Task Manager/Doowado - Task Manager/EditCommand.h
@@ -7,15 +7,16 @@ const string taskType = "t";
 class EditCommand : public Command
 {
 private:
-	string _entryType;
+	EntryType _entryType;
 	int _taskID;
 	string _newTitle;
+	ptime _newStartDate;
 	ptime _newStartTime;
+	ptime _newEndDate;
 	ptime _newEndTime;
-	ptime _newDueTime;
 
 public:
-	EditCommand(string, int, string, ptime, ptime, ptime);
+	EditCommand(EntryType, int, string, ptime, ptime, ptime, ptime);
 	~EditCommand();
 	void execute(Storage* data, Display* display);
 };

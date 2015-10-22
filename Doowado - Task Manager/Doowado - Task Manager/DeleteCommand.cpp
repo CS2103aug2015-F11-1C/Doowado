1d03f629b88fab672ab674a85130e7f0189f47e8
shelviadh <shelvia.ho@gmail.com>
2015-10-23 00:16:33 +0800

    implement generateFeedback() function in DeleteCommand

--- Doowado - Task Manager/Doowado - Task Manager/DeleteCommand.cpp
+++ Doowado - Task Manager/Doowado - Task Manager/DeleteCommand.cpp
@@ -1,6 +1,40 @@
 #include "DeleteCommand.h"
 
 
+void DeleteCommand::generateFeedback()
+{
+	_feedback.push_back(MESSAGE_SUCCESSFUL_DELETE);
+	string entryTypeString;
+	string indexString;
+	string startTimeString;
+	string endTimeString;
+	string title;
+
+	indexString = to_string(_taskID);
+
+	if (_entryType == event) {
+		entryTypeString = "E";
+		title = _eventDeleted->getName();
+		startTimeString = to_simple_string(_eventDeleted->getStartTime());
+		endTimeString = to_simple_string(_eventDeleted->getEndTime());
+
+		_feedback.push_back(entryTypeString + indexString);
+		_feedback.push_back(title);
+		_feedback.push_back(startTimeString);
+		_feedback.push_back(endTimeString);
+	}
+	else if (_entryType == task) {
+		entryTypeString = "T";
+		title = _taskDeleted->getName();
+		endTimeString = to_simple_string(_taskDeleted->getDueTime());
+
+		_feedback.push_back(entryTypeString + indexString);
+		_feedback.push_back(title);
+		_feedback.push_back(endTimeString);
+	}
+	
+}
+
 DeleteCommand::DeleteCommand(EntryType entryType, int displayIndex) {
 	_entryType = entryType;
 	_taskID = displayIndex;
@@ -14,11 +48,15 @@ DeleteCommand::~DeleteCommand()
 void DeleteCommand::execute(Storage* data, Display* display) {
 	//delete command
 	if (_entryType == event) {
-		Event* eventToDelete = display->retrieveEvent(_taskID);
-		data->deleteFromEventList(eventToDelete);
+		_eventDeleted = display->retrieveEvent(_taskID);
+		data->deleteFromEventList(_eventDeleted);
 	}
 	else if (_entryType == task) {
-		Task* taskToDelete = display->retrieveTask(_taskID);
-		data->deleteFromTaskLIst(taskToDelete);
+		_taskDeleted = display->retrieveTask(_taskID);
+		data->deleteFromTaskLIst(_taskDeleted);
 	}
+
+	generateFeedback();
+	display->updateCommandFeedback(_feedback);
 }
+
--- Doowado - Task Manager/Doowado - Task Manager/DeleteCommand.h
+++ Doowado - Task Manager/Doowado - Task Manager/DeleteCommand.h
@@ -1,12 +1,17 @@
 #pragma once
 #include "Command.h"
 
+const string MESSAGE_SUCCESSFUL_DELETE = "Successfully deleted!";
+
 class DeleteCommand: public Command {
 	private:
 		EntryType _entryType;
 		int _taskID;
 		Event* _eventDeleted;
 		Task* _taskDeleted;
+		vector<string> _feedback;
+
+		void generateFeedback();
 
 	public:
 		DeleteCommand(EntryType, int);

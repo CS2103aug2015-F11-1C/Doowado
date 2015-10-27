ae834de95b40342316dbbcc21970b2a5b52101be
shelviadh <shelvia.ho@gmail.com>
2015-10-27 15:28:11 +0800

    Updated Entry class to single model

--- Doowado - Task Manager/Doowado - Task Manager/Entry.cpp
+++ Doowado - Task Manager/Doowado - Task Manager/Entry.cpp
@@ -1,17 +1,77 @@
 #include "Entry.h"
 
 Entry::Entry() {
-	_name = "";
+	_title = "";
 }
 
-Entry::Entry(string name){
-	_name = name;
+Entry::Entry(string title, ptime endTime)
+{
+	_title = title;
+	_endTime = endTime;
+	_isDone = false;
+	_isConfirmed = true;
 }
 
-string Entry::getName(){
-	return _name;
+Entry::Entry(string title, ptime startTime, ptime endTime)
+{
+	_title = title;
+	_startTime = startTime;
+	_endTime = endTime;
+	_isDone = false;
+	_isConfirmed = true;
 }
 
-void Entry::setName(string newName){
-	_name = newName;
+Entry::Entry(string title){
+	_title = title;
+	_isDone = false;
+	_isConfirmed = true;
+}
+
+string Entry::getTitle(){
+	return _title;
+}
+
+ptime Entry::getStartTime()
+{
+	return _startTime;
+}
+
+ptime Entry::getEndTime()
+{
+	return _endTime;
+}
+
+bool Entry::isDone()
+{
+	return _isDone;
+}
+
+bool Entry::isConfirmed()
+{
+	return _isConfirmed;
+}
+
+void Entry::setTitle(string newTitle)
+{
+	_title = newTitle;
+}
+
+void Entry::setStartTime(ptime startTime)
+{
+	_startTime = startTime;
+}
+
+void Entry::setEndTime(ptime endTime)
+{
+	_endTime = endTime;
+}
+
+void Entry::setDone(bool doneStatus)
+{
+	_isDone = doneStatus;
+}
+
+void Entry::setConfirmed(bool confirmStatus)
+{
+	_isConfirmed = confirmStatus;
 }

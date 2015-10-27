ee98918b250145f7a3d66fe49cc689b1e9cea2f7
shelviadh <shelvia.ho@gmail.com>
2015-10-27 15:21:08 +0800

    created Logic class

---
+++ Doowado - Task Manager/Doowado - Task Manager/Logic.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Storage.h"
+#include "CommandBuilder.h"
+#include "Command.h"
+#include "Parser.h"
+#include "Display.h"
+
+static ptime currentTime(second_clock::local_time());
+
+using namespace std;
+
+class Logic
+{
+private:
+	Storage* _storage;
+	CommandBuilder* _cmdBuilder;
+	Parser* _parser;
+	Display* _display;
+
+public:
+	Logic();
+	~Logic();
+	void processCommand(string);
+	void initialiseProgram();
+};
+

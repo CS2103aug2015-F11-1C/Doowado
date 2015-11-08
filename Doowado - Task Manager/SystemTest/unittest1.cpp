31a6dad6633d791845bfbc98793e623bb84d8a56
diptivm <diptivm08@gmail.com>
2015-11-08 13:13:02 +0800

    Re-added systemTest project

---
+++ Doowado - Task Manager/SystemTest/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SystemTest
+{		
+	TEST_CLASS(AddTaskTest)
+	{
+	public:
+		
+		TEST_METHOD(AddTask)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
